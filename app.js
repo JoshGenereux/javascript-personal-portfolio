fdfbf05d8d4b28273942f7063ef6d8ac8cd088e8
JoshGenereux <josh.genereux13@gmail.com>
2022-01-14 15:01:49 -0700

    added footer

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const projects = document.getElementById('projects-btn')
 const links = document.getElementById('links-btn')
 const aTop = document.getElementById('about-home')
 const sTop = document.getElementById('skills-home')
+const pTop = document.getElementById('project-home')
+const fTop = document.getElementById('footer-home')
 
 about.addEventListener('click', ()=>{
     document.getElementById('about').scrollIntoView({
@@ -38,6 +40,27 @@ aTop.addEventListener('click', ()=>{
 
 sTop.addEventListener('click', ()=>{
     document.getElementById('name-sect').scrollIntoView({
+        behavior: "smooth",
+        block: "center"
+    })
+})
+
+fTop.addEventListener('click', ()=>{
+    document.getElementById('name-sect').scrollIntoView({
+        behavior: "smooth",
+        block: "center"
+    })
+})
+
+pTop.addEventListener('click', ()=>{
+    document.getElementById('name-sect').scrollIntoView({
+        behavior: "smooth",
+        block: "center"
+    })
+})
+
+links.addEventListener('click', ()=> {
+    document.getElementById('footer-div').scrollIntoView({
         behavior: "smooth",
         block: "center"
     })
